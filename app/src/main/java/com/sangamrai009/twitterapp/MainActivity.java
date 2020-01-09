f3cc942b92c3bb491d8a7322911b51b56c9a3e11
Sangam Rai <adambantawa@gmail.com>
2020-01-09 12:33:22 +0545

    Initial commit

---
+++ app/src/main/java/com/sangamrai009/twitterapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sangamrai009.twitterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
